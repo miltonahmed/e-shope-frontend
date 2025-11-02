8ce7d75ba4dd9b049fa2ad7b9b55fe1de09f69bb
Milton Ahmed <miltonahmed08@yahoo.com>
2025-11-02 09:24:05 +0400

    Feat: Container
    enhance Container component with responsive padding and className support

---
+++ src/Components/Common/Container.jsx
@@ -0,0 +1,10 @@
+
+const Container = ({children}) => {
+  return (
+    <div className="max-w-[1520px] mx-auto px-4 sm:px-6 lg:px-8">
+      {children}
+    </div>
+  );
+}
+
+export default Container
